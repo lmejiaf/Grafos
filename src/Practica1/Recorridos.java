71d92ace04829591fc53ebd13fbfb2d667cee9e6
Luis Mejia Blanco <lmejiaf@UNINORTEACAD.UNINORTE.EDU.CO>
2018-09-10 10:35:08 -0500

    recorridos grafos bfs y dfs

--- src/Practica1/Recorridos.java
+++ src/Practica1/Recorridos.java
@@ -5,6 +5,9 @@
  */
 package Practica1;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author lmejiaf
@@ -19,11 +22,11 @@ public class Recorridos {
     static boolean[] visitado = new boolean[matAd.length];
 
     public static void DFS(int u) {
-
+//como hacer un preOrden
         //origen u, destino v
         System.out.print(u + " ");
         visitado[u] = true;
-        for (int v = 0; v < matAd[0].length; v++) {
+        for (int v = 0; v < matAd.length; v++) {
             if (matAd[u][v] == 1) {
                 if (!visitado[v]) {
                     DFS(v);
@@ -35,9 +38,39 @@ public class Recorridos {
 
     }
 
+    public static void BFS(int u) {
+        // es como un recorrido por niveles
+
+        Queue<Integer> cola = new LinkedList<Integer>();//FIFO
+        visitado[u] = true;
+        cola.add(u);
+
+        while (!cola.isEmpty()) {
+            u = cola.remove();
+            System.out.print(u + " ");
+
+            for (int v = 0; v < matAd.length; v++) {
+                if (matAd[u][v] == 1) {
+                    if (!visitado[v]) {
+                        visitado[v] = true;
+                        cola.add(v);
+                    }
+                }
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
+        System.out.println("DFS");
         DFS(0);
-        
+        for (int i = 0; i < visitado.length; i++) {
+            visitado[i] = false;
+        }
+        System.out.println(" ");
+        System.out.println("BFS");
+        BFS(0);
     }
 
 }
